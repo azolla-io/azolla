259e7a13bd19c00d0b71458b40959452b455d780
Jianlong Zhong <zh.jianlong@gmail.com>
2025-09-08 22:12:44 -0700

    refactor: eliminate proto file duplication by referencing main proto directory

--- clients/rust/azolla-client/build.rs
+++ clients/rust/azolla-client/build.rs
@@ -1,10 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Use the main proto directory from the repository root
+    let proto_dir = "../../../proto";
+    
     tonic_build::configure()
         .build_server(false) // Client only, no server code
         .build_client(true)
         .compile(
-            &["proto/orchestrator.proto", "proto/common.proto"],
-            &["proto"],
+            &[
+                &format!("{}/orchestrator.proto", proto_dir),
+                &format!("{}/common.proto", proto_dir)
+            ],
+            &[proto_dir],
         )?;
     Ok(())
 }
